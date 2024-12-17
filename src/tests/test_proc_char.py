35448088231981751dc3acf89c514ab55a691a91
Jakob Dambon <jakob.dambon@gmail.com>
2024-12-17 17:35:52 +0100

    adding tokenizer and updating unicode removal function.

--- src/tests/test_proc_char.py
+++ src/tests/test_proc_char.py
@@ -3,12 +3,10 @@ from ..utils.proc_char import remove_unicode
 
 def test_remove_unicode() -> None:
     text1 = 'Some text'
-    text2 = ', more text'
-    text3 = '; final text.'
-    u1 = '\\u1234'
-    u2 = '\\u2042'
-    input1 = text1 + u1 + text2 + u2 + text3
-    expected_out = text1 + text2 + text3
+    text2 = ', more text.'
+    u1 = ''
+    input1 = text1 + u1 + text2
+    expected_out = text1 + text2
     input2 = expected_out
     assert remove_unicode(input1) == expected_out
     assert remove_unicode(input2) == expected_out
